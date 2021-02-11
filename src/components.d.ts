17c571bc56936744da4127f70ef7cfde493c9a2c
Josh Mabry <mabry1985@gmail.com>
2021-02-11 13:49:47 -0800

    skeleton out side drawer and style

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface JmSideDrawer {
+        "title": string;
     }
     interface MyComponent {
         /**
@@ -43,6 +44,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface JmSideDrawer {
+        "title"?: string;
     }
     interface MyComponent {
         /**
--- src/components/jm-side-drawer/jm-side-drawer.tsx
+++ src/components/jm-side-drawer/jm-side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'jm-side-drawer',
@@ -6,13 +6,18 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class JmSideDrawer {
+  @Prop() title: string;
 
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+        </header>
+        <main>
+          <slot />
+        </main>
+      </aside>
     );
   }
-
 }
